30dba695d4f0a95f1911b454bce2778e26580446
sudhakar <sudhakarmeenkeri@gmail.com>
2025-12-15 00:31:12 +0530

    worked on qa

--- src/app/service/questionAnswer.Service.ts
+++ src/app/service/questionAnswer.Service.ts
@@ -11,14 +11,14 @@ export class QuestionAnswerService {
     constructor(private http: HttpClient) { }
 
     createUserQA(userQA: any): Observable<any> {
-        const apiUrl = 'http://localhost:8080/api/qa/create';
-        // const apiUrl = 'https://dictionary-app-backend-9wm9.onrender.com/api/qa/create';
+        // const apiUrl = 'http://localhost:8080/api/qa/create';
+        const apiUrl = 'https://dictionary-app-backend-9wm9.onrender.com/api/qa/create';
         return this.http.post<any>(apiUrl, userQA);
     }
 
     getAllUserQA(): Observable<any> {
-        const apiUrl = 'http://localhost:8080/api/qa/getAllUserQA';
-        // const apiUrl = 'https://dictionary-app-backend-9wm9.onrender.com/api/qa/getAllUserQA';
+        // const apiUrl = 'http://localhost:8080/api/qa/getAllUserQA';
+        const apiUrl = 'https://dictionary-app-backend-9wm9.onrender.com/api/qa/getAllUserQA';
         return this.http.get<any>(apiUrl);
     }
 
