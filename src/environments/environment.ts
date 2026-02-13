80f8cddc5194d3839d84cf78b04e0f7a7731131a
sudhakar <sudhakarmeenkeri@gmail.com>
2026-02-13 23:13:19 +0530

    new changes

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,7 +5,8 @@ import { Environment } from './environment.model';
 
 export const environment: Environment = {
   production: false,
-  apiUrl: 'http://localhost:8080/api',
+  // apiUrl: 'http://localhost:8080/api',
+  apiUrl:'https://dictionaryappbackend-production.up.railway.app/api',
 
   analytics: {
     // Matches: http://localhost:8080/api/page-view/increment?pageName=HomePage
